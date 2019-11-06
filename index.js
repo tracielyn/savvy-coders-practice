8b50a7d1247eb6d471b4d239af2a13ab243e105a
Tracie Walka <tracielyn7@gmail.com>
2019-11-05 21:07:19 -0600

    Index for js

---
+++ index.js
@@ -0,0 +1,20 @@
+const myName = "Tracie Walka";
+
+//Template Literal
+console.log(`Hello World! This is ${myName}`);
+
+//Object Literal
+const me = {
+  name: "Tracie",
+  age: 33,
+  isBeingTruthful: false,
+  hairColor: "ombre",
+  eyeColor: "grey"
+};
+
+//We are using BRACKET NOTATION
+console.log(me["name"]);
+
+//We are using DOT NOTATION
+console.log(me.age);
+
